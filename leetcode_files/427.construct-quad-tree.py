46052ee72604a2e8783145836749e94f30c98beb
fang jiahao <74106740+wxb-gs@users.noreply.github.com>
2025-07-02 17:34:08 +0800

    x

--- leetcode_files/427.construct-quad-tree.py
+++ leetcode_files/427.construct-quad-tree.py
@@ -10,7 +10,7 @@
 
 # @lcpr-template-end
 # @lc code=start
-"""
+
 # Definition for a QuadTree node.
 class Node:
     def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
@@ -20,11 +20,30 @@ class Node:
         self.topRight = topRight
         self.bottomLeft = bottomLeft
         self.bottomRight = bottomRight
-"""
+
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
+        n = len(grid)
         
+        # 每一块的size, 右下角i, j
+        def dfs(size, i, j):
+            if size == 1:
+                return Node(grid[i][j], True, None, None, None, None)
+            size //= 2
+            tleft = dfs(size, i - size, j - size)
+            tright = dfs(size, i - size, j)
+            bleft = dfs(size, i, j - size)
+            bright = dfs(size, i, j)
+            # print(i , j)
+            if tleft.isLeaf and tright.isLeaf and bleft.isLeaf and bright.isLeaf:
+                if tleft.val == tright.val and tleft.val == bleft.val and bleft.val == bright.val:
+                    return Node(grid[i][j], True, None, None, None, None)
+            
+            return Node(1, False, tleft, tright, bleft, bright)
+        
+        return dfs(n, n - 1, n - 1)
+
 # @lc code=end
 
 
