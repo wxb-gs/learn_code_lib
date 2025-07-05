f6b56c75a4abd90799143331cd317d598a5b16ce
fang jiahao <74106740+wxb-gs@users.noreply.github.com>
2025-07-05 16:55:00 +0800

    letcode

--- leetcode_files/236.lowest-common-ancestor-of-a-binary-tree.py
+++ leetcode_files/236.lowest-common-ancestor-of-a-binary-tree.py
@@ -11,15 +11,49 @@
 # @lcpr-template-end
 # @lc code=start
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+import math
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
+    
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        u = p.val
+        v = q.val
+        val_to_node = {}
+        g = {}
+        def build(root, fa):
+            if not root:
+                return
+            val = root.val
+            val_to_node[val] = root
+            
+            if val not in g:
+                g[val] = set()                
+            if fa:
+                g[val].add(fa.val)
+            if root.left:
+                build(root.left, root)
+                g[val].add(root.left.val)
+            if root.right:
+                build(root.right, root)
+                g[val].add(root.right.val)
         
+        build(root, None)   
+        print(g)            
+        
+        n = len(g.keys())
+        f = {}
+        
+        def tarjan(u):
+            
+        
+        return val_to_node[tarjan(u)]
 # @lc code=end
 
 
